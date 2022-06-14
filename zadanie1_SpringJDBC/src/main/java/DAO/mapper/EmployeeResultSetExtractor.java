6f17d2f8651f53a0490b18eb0c2ad4a067e83036
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-06-14 09:06:52 +0200

    add IDao.java in DAO package, add EmployeeResultSetExcrator.java and EmployeeRowMapper.java in DAO.mappper package

---
+++ zadanie1_SpringJDBC/src/main/java/DAO/mapper/EmployeeResultSetExtractor.java
@@ -0,0 +1,23 @@
+
+package DAO.mapper;
+
+import domainmodel.Employee;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+
+public class EmployeeResultSetExtractor implements ResultSetExtractor {
+    @Override
+    public Object extractData(ResultSet rs) throws SQLException {
+        Employee emp= new Employee();
+        emp.setPracId(rs.getInt(1));
+        emp.setPracImie(rs.getString(2));
+        emp.setPracNazwisko(rs.getString(3));
+        emp.setPracWiek(rs.getInt(4));
+        emp.setPracNrTelefonu(rs.getString(5));
+        emp.setPracEmail(rs.getString(6));
+        return emp;
+       
+    }
+}
