533f3e36764b5f5100695a4779e0864dcc985796
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-06-14 09:39:26 +0200

    add methods deleteByID, create, selectAll, getByID, update in EmployeeDAO.java

--- zadanie1_SpringJDBC/src/main/java/DAO/EmployeeDAO.java
+++ zadanie1_SpringJDBC/src/main/java/DAO/EmployeeDAO.java
@@ -1,10 +1,13 @@
 package DAO;
 
+import DAO.mapper.EmployeeRowMapper;
 import domainmodel.Employee;
 import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class EmployeeDAO implements IDao {
+
     private DriverManagerDataSource dataSource;
 
     @Override
@@ -15,27 +18,51 @@ public class EmployeeDAO implements IDao {
 
     @Override
     public void deleteByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JdbcTemplate deleteByID = new JdbcTemplate(dataSource);
+        deleteByID.update("DELETE from pracownicy where prac_id= ?",
+                new Object[]{id});
     }
 
     @Override
     public void create(String imie, String nazw, int wiek, String nr_tel, String e_mail) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JdbcTemplate insert = new JdbcTemplate(dataSource);
+        insert.update("INSERT INTO pracownicy (prac_imie,prac_nazwisko,prac_wiek,prac_nr_telefonu,prac_email) VALUES(?,?,?,?,?)",
+                new Object[]{imie, nazw, wiek, nr_tel, e_mail});
     }
 
     @Override
     public List<Employee> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JdbcTemplate select = new JdbcTemplate(dataSource);
+        return select.query("select prac_id,prac_imie,prac_nazwisko,prac_wiek,prac_nr_telefonu,prac_email from pracownicy",
+                new EmployeeRowMapper());
     }
 
     @Override
     public Employee getByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JdbcTemplate select = new JdbcTemplate(dataSource);
+        String sql = "select prac_id,prac_imie,prac_nazwisko,prac_wiek,prac_nr_telefonu,prac_email from pracownicy where prac_id= ?";
+
+        return select.queryForObject(sql, new Object[]{id},
+                (rs, rowNum)
+                -> new Employee(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getString(5),
+                        rs.getString(6)
+                )
+        );
     }
 
     @Override
     public void update(int id, String imie, String nazw, int wiek, String nr_tel, String e_mail) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "update pracownicy\n"
+                + "set prac_imie= ?,prac_nazwisko= ?,prac_wiek= ?,prac_nr_telefonu= ?,prac_email= ?\n"
+                + "where prac_id= ?";
+        JdbcTemplate update = new JdbcTemplate(dataSource);
+        update.update(query, new Object[]{imie, nazw, wiek, nr_tel, e_mail, id});
+
     }
 
 }
--- zadanie1_SpringJDBC/src/main/java/com/mycompany/zadanie1_springjdbc/Main.java
+++ zadanie1_SpringJDBC/src/main/java/com/mycompany/zadanie1_springjdbc/Main.java
@@ -1,17 +1,30 @@
-
 package com.mycompany.zadanie1_springjdbc;
 
 import DAO.EmployeeDAO;
 import connect.SpringJdbcConfig;
-
+import domainmodel.Employee;
+import java.util.List;
 
 public class Main {
 
-    
     public static void main(String[] args) {
-        EmployeeDAO empDao=new EmployeeDAO();
-        SpringJdbcConfig sjc=new SpringJdbcConfig();
+        EmployeeDAO empDao = new EmployeeDAO();
+        SpringJdbcConfig sjc = new SpringJdbcConfig();
         empDao.setDataSource(sjc.mssqlDataSource());
+//        empDao.create("Adam", "Kowalski", 22, "123789546", "email@op.pl");
+//        empDao.create("Jan", "Kowalski", 22, "123789547", "jan@op.pl");
+//        List<Employee> list = empDao.selectAll();
+//        for (Employee emp : list) {
+//            System.out.print(emp.getPracId() + " ");
+//            System.out.println(emp.getPracImie() + " ");
+//            System.out.println(emp.getPracNazwisko() + " ");
+//            System.out.println(emp.getPracWiek() + " ");
+//            System.out.println(emp.getPracNrTelefonu() + " ");
+//            System.out.println(emp.getPracEmail() + " ");
+//        }
+//        Employee p1 = empDao.getByID(34);
+//        System.out.println(p1.getPracId() + " " + p1.getPracImie() + " " + p1.getPracEmail());
+//        empDao.update(1, "Ada", "Nowak", 22, "432561765", "ada@op.pl");
     }
-    
+
 }
